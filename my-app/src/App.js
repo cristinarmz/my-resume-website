8cb6e77133c5612e51dc37eb9774062511713a58
Cristina Ramírez Rosado <cristinaramirezrosado@gmail.com>
2024-07-02 01:38:48 -0600

    Added columns to showcase work

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 // src/App.js
 
 import React from 'react';
-import Navbar from './Navbar'; // Import the Navbar component
-import Section1 from './Section1'; // Import Section1 component
-import './App.css'; // Import App CSS if needed
+import Navbar from './Navbar'; 
+import Section1 from './Section1'; 
+import './App.css'; 
+import Work from './Work';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <div className="content">
         <Section1 />
+        <Work />
         
         <p>Scroll down to see the navbar stick at the top.</p>
       </div>
