d5ddd72c65e3c5b43224867c6b8c6625aba48670
Cristina Ramírez Rosado <cristinaramirezrosado@gmail.com>
2024-07-04 16:25:09 -0600

    Update

--- my-app/src/components/Section1.js
+++ my-app/src/components/Section1.js
@@ -13,9 +13,9 @@ const Section1 = () => {
       <div className="columns">
         <div className="column">
           <h3>Hi there! 👋🏼</h3>
-          <p className="intro">I’m Cristina,
-          <span style={{ fontWeight: 'bold' }}>Front-end Developer, UI/UX & Multimedia Designer,</span> 
-          focused on creating great products for <span className="strikethrough-word">users</span> people. </p>
+          <p className="intro">I’m Cristina, 
+          <span style={{ fontWeight: 'bold' }}> Front-end Developer, UI/UX & Multimedia Designer, </span> 
+           focused on creating great products for <span className="strikethrough-word">users</span> people. </p>
           <TextRotator />
         </div>
         <div className="column">
