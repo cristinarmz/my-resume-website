731a7eef3051309b0eb95ba5adc0845a498dd7e9
Cristina Ramírez Rosado <cristinaramirezrosado@gmail.com>
2024-07-09 14:11:57 -0600

    Changes to card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import './Card.css'; // Assuming you have a CSS file for Card styling
 
-const Card = ({ title, description, imageUrl, color }) => {
+const Card = ({ title, description, imageUrl, color, url }) => {
   const cardStyle = {
     backgroundColor: color,
   };
@@ -13,7 +13,9 @@ const Card = ({ title, description, imageUrl, color }) => {
       <img src={imageUrl} alt={title} className="card-image" />
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
+        <a className="card-description" href={url} target="_blank" rel="noopener noreferrer">
+                {description}
+            </a>
       </div>
     </div>
   );
--- src/components/Work.js
+++ src/components/Work.js
@@ -10,86 +10,97 @@ const Work = () => {
     {
       id: 1,
       title: 'Software Development Company',
-      description: 'Description of Project 1',
+      description: 'View Website',
       imageUrl: '/images/dacodes1.png',
-      color: '#040848' // Color for this card
+      url: 'https://www.dacodes.com',
+      color: '#040848' 
     },
     {
       id: 2,
       title: 'Marketing Agency Website',
-      description: 'Description of Project 2',
+      description: 'View Website',
       imageUrl: '/images/viralco.png',
-      color: '#EA4335' // Color for this card
+      url: 'https://www.viralco.mx',
+      color: '#EA4335'
     },
     {
       id: 3,
       title: 'Luxury Leather Repair Company',
-      description: 'Description of Project 3',
+      description: 'View Website',
       imageUrl: '/images/everleather.png',
-      color: '#E07D69' // Color for this card
+      url: 'https://www.everleather.co',
+      color: '#E07D69'
     },
     {
         id: 4,
         title: 'Holistic Coach Platform',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/rosysandoval.png',
-        color: '#C32361' // Color for this card
+        url: 'https://www.descubretuser.com',
+        color: '#C32361'
       },
       {
         id: 5,
         title: 'NGO Crowdfunding & Website',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/mexicoazul.png',
-        color: '#3F96FC' // Color for this card
+        url: 'https://www.mexicoazul.org',
+        color: '#3F96FC'
       },
       {
         id: 6,
         title: 'Wedding Invitation',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/ayc.png',
-        color: '#8A985D' // Color for this card
+        url: 'https://www.adrianaycarlos.com',
+        color: '#8A985D'
       },
       {
         id: 7,
         title: 'Tech Podcast',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/holamundo.png',
-        color: '#8C2CD3' // Color for this card
+        url: 'https://www.holamundopod.com',
+        color: '#8C2CD3'
       },
        {
         id: 8,
         title: 'Internal Knowledge Hub Platform',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/dacodes2.png',
-        color: '#15D6CF' // Color for this card
+        url: 'https://wiki.dacodes.com',
+        color: '#15D6CF' 
       },
           {
         id: 9,
         title: 'Pop Singers’ Website',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/prez.png',
-        color: '#F92056' // Color for this card
+        url: 'https://www.everleather.co',
+        color: '#F92056' 
       },
       {
         id: 10,
         title: 'Swimwear E-Commerce',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/sela.png',
-        color: '#AC322E' // Color for this card
+        url: 'https://selaswim.com',
+        color: '#AC322E' 
       },
       {
         id: 11,
         title: 'Global network of Accountants',
-        description: 'Description of Project 3',
+        description: 'Deprecated Project',
         imageUrl: '/images/iecnet.png',
-        color: '#013068' // Color for this card
+        color: '#013068' 
       },
       {
         id: 12,
         title: 'Baby Clothing E-Commerce',
-        description: 'Description of Project 3',
+        description: 'View Website',
         imageUrl: '/images/bicos.png',
-        color: '#996989' // Color for this card
+        url: 'https://www.bicosebicos.com',
+        color: '#996989' 
       },
       
   ];
@@ -102,6 +113,7 @@ const Work = () => {
             key={item.id}
             title={item.title}
             description={item.description}
+            url={item.url}
             imageUrl={item.imageUrl}
             color={item.color}
           />
